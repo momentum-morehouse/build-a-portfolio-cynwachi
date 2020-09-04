d594ae6cf5b711f1d991aa92452a0c918f88c32a
cynwachi <christinenwachi@gmail.com>
2020-09-03 21:37:45 -0500

    resume

--- views/main.js
+++
@@ -1,7 +0,0 @@
-var fields = {};
-document.addEventListener("DOMContentLoaded", function() {
-    fields.fName = document.getElementById('firstName');
-    fields.lName = document.getElementById('lastName');
-    fields.email = document.getElementById('email');
-    fields.subject = document.getElementById('subject');
-   })
